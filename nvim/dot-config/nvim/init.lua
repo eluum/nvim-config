d1edd9a9229391191f3cf191eaaa8d9adcdaa9d9
eluum <eluum-contact@proton.me>
2025-10-06 00:18:13 -0700

    removing accidental tabstop change

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -11,7 +11,7 @@ vim.opt.wrap = false
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 2
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.backup = false
 vim.opt.writebackup = false
@@ -35,7 +35,7 @@ vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 local opts = { silent = true }
 
--- Window commands
+-- window commands
 vim.keymap.set('n', '<leader>h', ':wincmd h<CR>', opts)
 vim.keymap.set('n', '<leader>j', ':wincmd j<CR>', opts)
 vim.keymap.set('n', '<leader>k', ':wincmd k<CR>', opts)
@@ -43,31 +43,31 @@ vim.keymap.set('n', '<leader>l', ':wincmd l<CR>', opts)
 vim.keymap.set('n', '<leader>q', ':wincmd q<CR>', opts)
 vim.keymap.set('n', '<leader>Q', ':q!<CR>', opts)
 
--- Faster splitting
+-- faster splitting
 vim.keymap.set('n', '<leader>H', ':wincmd v<bar>wincmd h<CR>', opts)
 vim.keymap.set('n', '<leader>L', ':wincmd v<CR>:wincmd l<CR>', opts)
 vim.keymap.set('n', '<leader>J', ':wincmd s<bar>wincmd j<CR>', opts)
 vim.keymap.set('n', '<leader>K', ':wincmd s<CR>:wincmd k<CR>', opts)
 
--- File explorer
+-- file explorer
 vim.keymap.set('n', '<leader>e', ':Vexplore!<CR>', opts)
 vim.keymap.set('n', '<leader>E', ':Ex<CR>', opts)
 vim.keymap.set('n', '<leader>t', ':Texplore<CR>', opts)
 vim.keymap.set('n', '-', ':Ex<CR>', opts)
 vim.g.netrw_banner = 0
 
--- Terminal
+-- terminal
 vim.keymap.set('n', '<leader>;', ':bot split<bar>resize 20<bar>terminal<CR>i', opts)
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
 vim.keymap.set('t', '<C-q>', '<C-\\><C-n>:bd!<CR>', opts)
 
--- Misc
+-- misc
 vim.keymap.set('n', '<leader>\\', ':noh<CR>', opts)
 vim.keymap.set('n', '<tab>', 'I<tab><esc>', opts)
 vim.keymap.set('n', '<s-tab>', 'I<backspace><esc>', opts)
 vim.keymap.set('n', '<leader>=', 'ggVG=\'\'', opts)
 
--- Copy paste
+-- copy paste
 vim.keymap.set('n', '<leader>p', '"0p', opts)
 vim.keymap.set('v', '<leader>p', '"0p', opts)
 vim.keymap.set('n', '<leader>P', '"+p', opts)
@@ -75,7 +75,7 @@ vim.keymap.set('v', '<leader>P', '"+p', opts)
 vim.keymap.set('n', '<C-V>P', '"+p', opts)
 vim.keymap.set('v', '<C-V>P', '"+p', opts)
 
--- language specific settings
+-- language specific settings --
 vim.api.nvim_create_autocmd('FileType', {
     pattern = {'gleam'},
     callback = function()
@@ -84,5 +84,5 @@ vim.api.nvim_create_autocmd('FileType', {
     end
 })
 
--- setup colorscheme
+-- setup colorscheme --
 require('colormemory').setup('default', 'dark')
